8be110b19948e825d34a5b6fa4e89c1c1c70f568
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-10-31 09:41:29 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Form from "./components/Form";
 
 const API_KEY = "a6fa2d4513b2118e543ef7b684a8de4d";
+//https://www.food2fork.com/about/api
 
 
 
@@ -17,11 +18,11 @@ class App extends Component {
   */
   getRecipe = async (e) =>{
     const recipeName = e.target.elements.recipeName.value;
-    e.preventDefault(); // prevent refreshing
+    e.preventDefault(); // prevent refreshing : submit 버튼 누르면 auto-page-refresh 되기때문에 그거 막는거
     const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`);
     //console.log(recipeName);
 
-    const data = await api_call.json();
+    const data = await api_call.json(); //json 파일로 읽어
     //console.log(data.recipes[0].recipe_id);
     this.setState({recipes:data.recipes});
     console.log(this.state.recipes);
